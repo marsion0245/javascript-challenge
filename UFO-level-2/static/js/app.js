c60c856363b2529a1795e5c98f4d5bb743c82086
Martin Hrbac <martin.hrbac@siemens.com>
2019-09-17 12:39:24 -0400

    finished cascading filter

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -8,6 +8,8 @@
 (function(){
 	// This is IIFE
 	
+	const ALL = "*";
+	
 	// A bit of text formating
 	data.forEach( row  => { 
 		row.state = row.state.toUpperCase();
@@ -25,34 +27,67 @@
 	const stringValues = ['datetime', 'city', 'shape'];
 
 	// Activate string filters
-	stringValues.forEach(el => d3.select("#f_" + el).on("keyup", () => filterByString(el)).on("keypress", () => cancelEvent()));
+	stringValues.forEach(el => d3.select("#f_" + el).on("keyup", () => filterByRegex(el)).on("keypress", () => cancelEvent()));
 
-	// Execute filter - this is formality since the filters are executed on each button up
-	d3.select("#filter-btn").on("click", () => stringValues.forEach(el => filterByString(el)));
+	// Activate dropdown filters
+	// Country
+	let countryList = Array.from( new Set(data.map(v => v.country))).sort(); // Unique sorted list
+	countryList.unshift(ALL);
+	d3.select("#li-country").append("select").attr("id", "f_country").selectAll("option").data(countryList).enter().append("option").text(c=>c).attr("value", c=>c);
+
+	// State - default value is *
+	let stateListDd = d3.select("#li-state").append("select").attr("id", "f_state");
+	stateListDd.selectAll("option").data([ALL]).enter().append("option").text(s=>s).attr("value", s=>s);
 	
-	// Cancel filter
+	d3.select("#f_country").on("change", () => {
+		let filter = d3.select("#f_country").node().value;
+		filter !== ALL ? rows.classed('f_country', p => p.country !== filter) : resetFilter('f_country');
+
+		// update states list based on selected country
+		let statesList = [ALL];
+		if(filter !== ALL){
+			statesList = statesList.concat(Array.from( new Set(data.filter( c => c.country === filter).map(v => v.state))).sort()); // Unique sorted list
+		}
+		document.getElementById("f_state").innerHTML = "";
+		stateListDd.selectAll("option").data(statesList).enter().append("option").text(s=>s).attr("value", s=>s);
+	});
+
+	d3.select("#f_state").on("change", () => {
+		let filter = d3.select("#f_state").node().value;
+		filter !== ALL ? rows.classed('f_state', p => p.state !== filter) : resetFilter('f_state');
+	});
+
+	// Execute filter - call from button click - this is formality since the filters are executed on each button up
+	d3.select("#filter-btn").on("click", () => stringValues.forEach(el => filterByRegex(el)));
+	
+	// Cancel filter - call from button click
 	d3.select("#cancel-btn").on("click", () => {
 		stringValues.forEach(el =>{
 			d3.select("#f_" + el).node().value = ""; 
-			filterByString(el);
+			filterByRegex(el);
 		});
+		d3.select("#f_country").node().value = ALL;
+		d3.select("#f_state").node().value = ALL;
+		resetFilter("f_country");
+		resetFilter("f_state");
 	});
-	
+		
 	const cancelEvent = () => {
 		if(d3.event.keyCode === 13){ 
-			// pressed Enter
+			// Enter pressed
 			d3.event.preventDefault();
 			d3.event.stopPropagation();
 		}
 	};	
 	
-	// Date filter
-	const filterByString = (attrName) => {
+	// Filter by string
+	const filterByRegex = (attrName) => {
 		// Get filter value
 		let filter = d3.select(`#f_${attrName}`).node().value;
-		// let regex = new RegExp(".*" + filter + ".*");
-		(Boolean(filter)) ? rows.classed(`f_${attrName}`, p => p[attrName].search(new RegExp(".*" + filter + ".*"))) : rows.classed(`f_${attrName}`, p => false);
+		(Boolean(filter)) ? rows.classed(`f_${attrName}`, p => p[attrName].search(new RegExp(".*" + filter + ".*"))) : resetFilter(`f_${attrName}`); // rows.classed(`f_${attrName}`, p => false);
 	};
-
+	
+	const resetFilter = param => rows.classed(param, false);
+	
 })();
 
